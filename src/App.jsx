fc3f94043a1afd1f44ca16302a718b742213dc40
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-04-10 23:36:55 +0530

    Navbar Done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './app.scss'
-import Dock from './components/Dock'
+import Dock from './components/Dock/Dock'
+import Nav from './components/Nav/Nav'
 
 const App = () => {
   return (
     <main>
+      <Nav />
       <Dock />
     </main>
   )
