07e26b15031640c5b641027cfb08e93d761dd0d4
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-04-11 23:16:17 +0530

    windows drag and drop feature created using react-rnd library

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './app.scss'
 import Dock from './components/Dock/Dock'
 import Nav from './components/Nav/Nav'
+import MacWindow from './components/windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav />
+      <MacWindow>
+        <h1>efdf</h1>
+      </MacWindow>
       <Dock />
     </main>
   )
